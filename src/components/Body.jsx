68200e4b251d5bd811bfb78ec60c6a6d91fc5892
Daniel Campos <danielgcampos.04@gmail.com>
2021-12-27 22:01:47 -0300

    desnio del contenedor de la imagen hecho para mobile

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import iconNext from '../../public/icon-next.svg'
+import iconPrevious from '../../public/icon-previous.svg'
+import iconMinus from '../../public/icon-minus.svg'
+import iconPlus from '../../public/icon-plus.svg'
+import iconCart from '../../public/icon-cart.svg'
+import { ImageContain } from '../styles/styledBody'
 const Body = () => {
+    const [image,setImage] = useState(1)
     return (
         <div>
+            <ImageContain num={`${image}`}>
+                <img src={iconPrevious} alt="" />
+                <img src={iconNext} alt="" />
+            </ImageContain>
             <div>
-                <button></button>
-                <img src="" alt=""/>
-                <button></button>
-            </div>
-            <div>
-                <p></p>
-                <h2></h2>
-                <p></p>
+                <p>SNEAKER COMPANY</p>
+                <h2>Fall Limited Edition Sneakers</h2>
+                <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber oter sole they'll withstand everything the weather can offer.</p>
                 <div>
-                    <p></p>
-                    <span></span>
-                    <span></span>
+                    <p>$125.00</p>
+                    <span>50%</span>
+                    <span>$250.00</span>
                 </div>
                 <div>
-                    <span></span>
-                    <p></p>
-                    <span></span>
+                    <img src={iconMinus} alt="" />
+                    <p>0</p>
+                    <img src={iconPlus} alt="" />
                 </div>
-                <button></button>
+                <button>
+                    <img src={iconCart} alt="" />
+                    Add to cart
+                </button>
             </div>
         </div>
     )
--- src/styles/styledBody.js
+++ src/styles/styledBody.js
@@ -14,3 +14,31 @@ export const StyleGlobal = createGlobalStyle`
     }
 `
 
+
+export const ImageContain = styled.div`
+    /* width: 100%; */
+    height: 350px;
+    margin-top:30px;
+    object-fit: cover;
+    background:url(${props => `../../public/image-product-${props.num}-thumbnail.jpg`}) no-repeat;
+    background-position:50% 50%;
+    background-size:100% 350px;
+    position: relative;
+    img{
+        position: absolute;
+        background:#fff;
+        top:50%;
+        transform: translateY(-50%);
+        width: 18px;
+        height: 18px;
+        padding:10px;
+        border-radius: 50%;
+        cursor: pointer;
+    }
+    img:first-child{
+        left:15px;
+    }
+    img:nth-child(2){
+        right:15px;
+    }
+`
